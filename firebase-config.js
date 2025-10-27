593cab2c705001dfff2eb1ad6aa674bdc29b91d5
BetterCallSaulAtlas <sherman@atlassian.com>
2025-10-28 07:52:38 +1100

    Configure Firebase with project credentials
    - Add Firebase project: meerkats-74de5
    - Configure Firestore connection
    - Ready for Firestore database setup

--- firebase-config.js
+++ firebase-config.js
@@ -4,13 +4,13 @@
 // Project Settings > General > Your apps > Web app > Config
 
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT_ID.appspot.com",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID",
-  measurementId: "YOUR_MEASUREMENT_ID" // Optional
+  apiKey: "AIzaSyAlH1akwhAgGwAN8ll2Q5Ytyg0izzVogsM",
+  authDomain: "meerkats-74de5.firebaseapp.com",
+  projectId: "meerkats-74de5",
+  storageBucket: "meerkats-74de5.firebasestorage.app",
+  messagingSenderId: "1035049140619",
+  appId: "1:1035049140619:web:171f5902856196c67aaf8e",
+  measurementId: "G-W7EEBNDNG9"
 };
 
 // Initialize Firebase
